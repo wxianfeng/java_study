2673c4e4173cbb9adbb5eeda8b1d8001afa16803
wxianfeng <wang.fl1429@gmail.com>
2021-04-19 01:28:20 +0800

    List SubList

---
+++ src/main/java/com/wxianfeng/open/list/SubListTest.java
@@ -0,0 +1,23 @@
+package com.wxianfeng.open.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author haomiao.wxf
+ * @date 2021/04/19 12:20 AM
+ */
+public class SubListTest {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        int i = 0;
+        while (i < 10) {
+            i++;
+            list.add(i);
+        }
+        // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        System.out.println(list);
+        // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        System.out.println(list.subList(0, 10));
+    }
+}
